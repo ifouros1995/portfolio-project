9da422c26a6572d380c737f15a865caf7a0aea8e
Ifouros <jimadamis95@yahoo.com>
2020-12-19 02:14:15 +0200

    All blogs and static root

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url, include
+
 import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.home , name='home'),
+    path('blog/', include('blog.urls')),
 
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
